3378ca346a7234478d1eece44e2cd3f9a26fc2fe
BadassHenkka <henri.kestio@gmail.com>
2019-03-28 13:04:30 +0200

    Changed font, added background image and top+bottom coloring

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# productdev
-group project for school
+# Kaitsen
+
+### Small steps to change your life
+
+This app is being developed for a product developement course in TUAS. It's designed to be a time management app for those who are new to schedules and proper time management. 
+
+
+
+#### Installation:
+
+```$ npm install --save expo-cli react-native-calendars```
+
+```$ npm install```
---
+++ screens/HomeScreen.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#FA8072',
+    },
+    headerTitleStyle: {
+      fontSize: 40,
+      fontWeight: '200',
+      fontFamily: 'glades',
+      color: '#fffff0'
+    },
+  };
+
+  render() {
+    return (
+    <ImageBackground source={require('../assets/images/kaitsenbackground.png')} style={styles.container}>
+      <Text style={styles.text}>Thank you for downloading Kaitsen</Text> 
+      <Text style={styles.text}>Open notes to get started</Text>
+    </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  text: {
+    textAlign: 'center',
+    marginLeft: 50,
+    marginRight: 50,
+    color: '#fffff0',
+    fontFamily: 'glades',
+    fontSize: 20
+  }
+});

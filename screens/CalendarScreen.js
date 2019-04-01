92811a3c8029b75b363cc86a6b744b1fe907ca1d
Thoumey <thomas.borgelin@gmail.com>
2019-04-01 11:06:05 +0300

    calendar now displays correct date

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -31,7 +31,8 @@ export default class CalendarScreen extends React.Component {
       <Agenda
         items={this.state.items}
         loadItemsForMonth={this.loadItems.bind(this)}
-        selected={'2017-05-16'}
+        selected={Date()}
+        minDate={'2017-04-20'}
         renderItem={this.renderItem.bind(this)}
         renderEmptyDate={this.renderEmptyDate.bind(this)}
         rowHasChanged={this.rowHasChanged.bind(this)}

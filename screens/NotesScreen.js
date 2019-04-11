8d948f77bb7056301714ef525db8fd6084eb0af5
Thoumey <thomas.borgelin@gmail.com>
2019-04-11 23:31:24 +0300

    Fixed status bar overlapping and changed some images in the /assets/ folder

--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, TextInput, Button, ListView, ImageBackground } from 'react-native';
-import { Constants } from 'expo';
+import { 
+  View, 
+  StyleSheet, 
+  ImageBackground,
+	StatusBar,
+	ActivityIndicator,
+	ScrollView,
+  AsyncStorage 
+} from 'react-native';
+import uuid from 'uuid/v1';
+
+import Input from '../components/Input';
+import List from '../components/List';
+import DeleteButton from '../components/DeleteButton';
+import SubTitle from '../components/SubTitle';
 
 export default class NotesScreen extends React.Component {
   static navigationOptions = {
     title: 'Notes',
     headerStyle: {
+      marginTop: StatusBar.currentHeight , //jättää statuspalkin pituuden verran tilaa yläreunaan, jotta logo ei jää sen alle
       backgroundColor: '#FA8072',
     },
     headerTitleStyle: {
@@ -15,115 +29,195 @@ export default class NotesScreen extends React.Component {
       color: '#fffff0'
     },
   };
-
-  constructor(props) {
-    super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      inputValue: '',
-      dataSource: ds.cloneWithRows([]),
-    };
-    this._handleTextChange = this._handleTextChange.bind(this);
-    this._handleDeleteButtonPress = this._handleDeleteButtonPress.bind(this);
-  }
-  _handleTextChange = (value) => {
-    const inputValue = value;
-    this.setState(() => ({
-      inputValue,
-    }));
-  }
-  _handleSendButtonPress = () => {
-    if (!this.state.inputValue) {
-      return;
-    }
-    const textArray = this.state.dataSource._dataBlob.s1;
-    textArray.push(this.state.inputValue);
-    this.setState(() => ({
-      dataSource: this.state.dataSource.cloneWithRows(textArray),
-      inputValue: '',
-    }));
+  state = {
+    inputValue: '',
+    loadingItems: false,
+    allItems: {},
+    isCompleted: false
   };
-  _handleDeleteButtonPress = (id) => {
-    this.setState((a) => {
-      const newItem = a.dataSource._dataBlob.s1.filter((item, i) => (parseInt(id) !== i));
-      return {
-        dataSource: this.state.dataSource.cloneWithRows(newItem),
-      }
+  
+
+  componentDidMount = () => {
+    this.loadingItems();
+  };
+
+  newInputValue = value => {
+    this.setState({
+      inputValue: value
     });
   };
 
+  loadingItems = async () => {
+    try {
+      const allItems = await AsyncStorage.getItem('Todos');
+      this.setState({
+        loadingItems: true,
+        allItems: JSON.parse(allItems) || {}
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  onDoneAddItem = () => {
+    const { inputValue } = this.state;
+    if (inputValue !== '') {
+      this.setState(prevState => {
+        const id = uuid();
+        const newItemObject = {
+          [id]: {
+            id,
+            isCompleted: false,
+            text: inputValue,
+            createdAt: Date.now()
+          }
+        };
+        const newState = {
+          ...prevState,
+          inputValue: '',
+          allItems: {
+            ...prevState.allItems,
+            ...newItemObject
+          }
+        };
+        this.saveItems(newState.allItems);
+        return { ...newState };
+      });
+    }
+  };
+
+  deleteItem = id => {
+    this.setState(prevState => {
+      const allItems = prevState.allItems;
+      delete allItems[id];
+      const newState = {
+        ...prevState,
+        ...allItems
+      };
+      this.saveItems(newState.allItems);
+      return { ...newState };
+    });
+  };
+
+  completeItem = id => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        allItems: {
+          ...prevState.allItems,
+          [id]: {
+            ...prevState.allItems[id],
+            isCompleted: true
+          }
+        }
+      };
+      this.saveItems(newState.allItems);
+      return { ...newState };
+    });
+  };
+
+  incompleteItem = id => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        allItems: {
+          ...prevState.allItems,
+          [id]: {
+            ...prevState.allItems[id],
+            isCompleted: false
+          }
+        }
+      };
+      this.saveItems(newState.allItems);
+      return { ...newState };
+    });
+  };
+
+  deleteAllItems = async () => {
+    try {
+      await AsyncStorage.removeItem('Todos');
+      this.setState({ allItems: {} });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  saveItems = newItem => {
+    const saveItem = AsyncStorage.setItem('Todos', JSON.stringify(newItem));
+  };
+
   render() {
+    const { inputValue, loadingItems, allItems } = this.state;
+  
     return (
       <ImageBackground source={require('../assets/images/kaitsenbackground.png')} style={styles.container}>
-        <View style={styles.formView}>
-          <TextInput
-            style={styles.inputForm}
-            value={this.state.inputValue}
-            onChangeText={this._handleTextChange}
-            placeholder="New task"
-            maxLength={30}
-          />
-          <Button
-            title="Add"
-            onPress={this._handleSendButtonPress}
-          />
-        </View>
-        <ListView
-          enableEmptySections={true}
-          style={styles.listView}
-          dataSource={this.state.dataSource}
-          renderRow={(rowData, sectionID, rowID) => {
-            const handleDelete = () => {
-              return this._handleDeleteButtonPress(rowID);
-            }
-            return (
-              <View style={styles.todoItem}>
-                <Text style={styles.todoText}>{rowData}</Text>
-                <Button
-                  title="Delete"
-                  onPress={handleDelete}
-                />
-              </View>
-              );
-            }
-          }
-        />
+        <StatusBar barStyle="light-content" />
+				<View style={styles.inputContainer}>
+					<SubTitle subtitle={"What's Next?"} />
+					<Input
+						inputValue={inputValue}
+						onChangeText={this.newInputValue}
+						onDoneAddItem={this.onDoneAddItem}
+					/>
+				</View>
+				<View style={styles.list}>
+					<View style={styles.column}>
+						<SubTitle subtitle={'Recent Notes'} />
+						<View style={styles.deleteAllButton}>
+							<DeleteButton deleteAllItems={this.deleteAllItems} />
+						</View>
+					</View>
+
+					{loadingItems ? (
+						<ScrollView contentContainerStyle={styles.scrollableList}>
+							{Object.values(allItems)
+								.reverse()
+								.map(item => (
+									<List
+										key={item.id}
+										{...item}
+										deleteItem={this.deleteItem}
+										completeItem={this.completeItem}
+										incompleteItem={this.incompleteItem}
+									/>
+								))}
+						</ScrollView>
+					) : (
+						<ActivityIndicator size="large" color="white" />
+					)}
+				</View>
       </ImageBackground>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#eee',
-  },
-  formView: {
-    borderBottomWidth: 1,
-    borderColor: '#ccc',
-    paddingBottom: 8,
-  },
-  inputForm: {
-    backgroundColor: '#fff',
-    width: 320,
-    height: 40,
-    padding: 8,
-    marginBottom: 8,
-  },
-  todoItem: {
-    alignItems: 'center',
-    padding: 8,
-    width: 320,
-    borderBottomWidth: 1.5,
-    borderColor: '#e0e0e0',
-    backgroundColor: '#fff',
-    flex: 1,
-    flexDirection: 'row',
-  },
-  todoText: {
-    flex: 1,
-  },
+	container: {
+		flex: 1
+	},
+	centered: {
+		alignItems: 'center'
+	},
+	inputContainer: {
+		marginTop: 40,
+		paddingLeft: 15
+	},
+	list: {
+		flex: 1,
+		marginTop: 70,
+		paddingLeft: 15,
+		marginBottom: 10
+	},
+	scrollableList: {
+		marginTop: 15
+	},
+	column: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between'
+	},
+	deleteAllButton: {
+		marginRight: 40
+	}
 });
